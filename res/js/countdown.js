0760ae42294074379f354dd0c88648dff3882ffb
Arpan Adlakhiya <40575581+mrr0b0t-0x1@users.noreply.github.com>
2021-04-24 13:19:35 +0530

    Early Bird countdown

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -2,9 +2,9 @@ $('.countdown-timer').countdown({
     date: "May 8, 2021 10:00:00",
 
     render: function (data) {
-        $(".days .value").html(this.leadingZeros(data.days, 2));
-        $(".hours .value").html(this.leadingZeros(data.hours, 2));
-        $(".minutes .value").html(this.leadingZeros(data.min, 2));
-        $(".seconds .value").html(this.leadingZeros(data.sec, 2));
+        $(".countdown-timer .days .value").html(this.leadingZeros(data.days, 2));
+        $(".countdown-timer .hours .value").html(this.leadingZeros(data.hours, 2));
+        $(".countdown-timer .minutes .value").html(this.leadingZeros(data.min, 2));
+        $(".countdown-timer .seconds .value").html(this.leadingZeros(data.sec, 2));
     }
 });
